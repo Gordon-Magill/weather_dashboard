0b4be190c636a22b166930ab4f17624303fa3acb
Gordon-Magill <gordon.magill@gmail.com>
2022-09-02 19:00:10 -0700

    Added API key, minor HTML formatting

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var weatherAPIKey = '81f2953a65aee813b28e36a32ee00ddb';
+
+// Event handler for city search
+
+
+// Add city card to city list
+
+// Alert user about bad search
+
+// Populate current conditions
+
+// Populate single future forecast
+
+// Populate all 5 days of forecast
