cb2c9f867a554a961acb9ce81e4cd9acd1f503bd
Gordon-Magill <gordon.magill@gmail.com>
2022-09-06 03:25:49 -0700

    Updated comments, minor styling, updated README

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
+// Don't you just love hard-coded API keys right in the client-side code? No backend for this app.
 var WEATHER_API_KEY = "b8628e5537299dd92c268cb43152d77d";
 
-// Helper function for presenting data
+// Helper function for rounding numbers, useful for presenting data nicely in the forecast cards
 // Based off of https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-decimal-places-if-necessary
 function roundSigFigs(numberArg, sigFigs) {
   return (
@@ -11,34 +12,27 @@ function roundSigFigs(numberArg, sigFigs) {
 // Event handler for city search
 var btnSearchCities = $("#btnSearchCities");
 var formSearchCities = $("#formSearchCities");
-
 btnSearchCities.on("click", function (event) {
   event.preventDefault();
   getConditions(formSearchCities.val());
-  // getForecastConditions(formSearchCities.val())
 });
 
 // Populate the city list on the left of the screen with cities stored in localStorage
 var leftBarContainer = $("#leftBarContainer");
 var cityList = JSON.parse(localStorage.getItem("cityList")) || [];
 function populateSearchHistory() {
-  // console.log('Creating city list on the left based on:');
-  // console.log(cityList)
-  // console.log(cityList.length)
   if (cityList.length > 0) {
     for (j = 0; j < cityList.length; j++) {
-      // console.log('Entering loop '+j);
       addCityCard(cityList[j], true);
-      // console.log('Exiting loop '+j)
     }
   }
 }
+
+// Do this populating right at the start of page load
 populateSearchHistory();
 
-// Add city card to city list on left of screen
+// Function to add city card to city list on left of screen
 function addCityCard(cityName, forceCreate) {
-  // console.log('Triggered addCityCard')
-
   // If the city card already exists, do nothing
   for (i = 0; i < cityList.length; i++) {
     if (cityList[i] === cityName && !forceCreate) {
@@ -48,10 +42,7 @@ function addCityCard(cityName, forceCreate) {
     }
   }
 
-  // If it's a new city, add a card for it
-  // Update city list
-  // console.log(`Creating city card for ${cityName}`)
-
+  // If there isn't a city of the same name already in the list, add it to the list
   var cityNameFlag = true;
   for (i = 0; i < cityList.length; i++) {
     if (cityList[i] == cityName) {
@@ -75,23 +66,19 @@ function addCityCard(cityName, forceCreate) {
 
   // Create bootstrap card body
   var newCityBody = $("<div>");
-  newCityBody.addClass("card-body p-1 cityCard");
+  newCityBody.addClass("card-body custom-card-body p-1");
   newCityBody.text(cityName);
 
   // Append elements to render new card
   newCityCard.append(newCityBody);
   leftBarContainer.append(newCityCard);
   localStorage.setItem("cityList", JSON.stringify(cityList));
-  // console.log('Finished making card for '+cityName)
 }
 
 //API call for current conditions
 function getConditions(cityName) {
-  var lat = 51.5085; //London
-  var lon = -0.1257; //London
-  var part = "hourly,daily";
+  // Fancy string formatting to create the URL
   var apiUrlCityCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${WEATHER_API_KEY}&units=metric`;
-  // var apiUrlCityCurrent = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&part=${part}`;
 
   fetch(apiUrlCityCurrent)
     .then(function (response) {
@@ -99,6 +86,7 @@ function getConditions(cityName) {
       console.log("Response:");
       console.log(response);
 
+      //   If the response is good, display the right results area and add a city card on the left
       if (response.status === 200) {
         $(".customRightArea").css("display", "block");
         addCityCard(cityName, false);
@@ -109,20 +97,24 @@ function getConditions(cityName) {
       return response.json();
     })
     .then(function (data) {
-      getExtendedUVData(data);
+      //Use this data to initiate the process of updating the page content
+      updatePageContent(data);
     });
 }
 
 // Update page with current conditions
-function getExtendedUVData(data) {
+function updatePageContent(data) {
   console.log("Response JSON:");
   console.log(data);
+
+  //   Extract current conditions from JSON
   var curTemp = data.main.temp;
   var curHumidity = data.main.humidity;
   var curWindSpd = data.wind.speed;
   var cityName = data.name;
 
   // Separate potentially paid API call just to get UVI...ugh...
+  //   This API call requires lat/lon, which can be acquired from the city search API response for a given city
   var lat = data.coord.lat;
   var lon = data.coord.lon;
   var apiUrlLatLon = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`;
@@ -134,6 +126,7 @@ function getExtendedUVData(data) {
       console.log(data);
       var curUVI = data.current.uvi;
 
+      //   Finally update the current conditions card now that UVI is available
       renderCurrentConditions(
         data,
         curTemp,
@@ -142,10 +135,12 @@ function getExtendedUVData(data) {
         cityName,
         curUVI
       );
+      //   Use the new API to call's data to additionally populate the forecast conditions
       renderForecastConditions(data);
     });
 }
 
+// Helper function to update current conditions card
 function renderCurrentConditions(
   data,
   curTemp,
@@ -154,25 +149,31 @@ function renderCurrentConditions(
   cityName,
   curUVI
 ) {
+  // Setting the name of the city at the top of the results
   var cityNameTitle = $("#cityNameTitle");
   cityNameTitle.text(cityName);
 
+  //   Update card with date in correct format
   var curDateEl = $("#curDateEl");
   curDateEl.text(moment().format("MMMM Do") + "\n(Current conditions)");
 
+  //   Retrieve and set the correct weather icon using the API response
   var curIconEl = $("#curIconEl");
   curIconEl.attr(
     "src",
     `http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`
   );
 
+  //   Update the card with a rounded temperature
   var curTemperatureEl = $("#curTemperatureEl");
   var roundedCurTemp = roundSigFigs(curTemp, 1);
   curTemperatureEl.text(`Temperature: ${roundedCurTemp}\u2103`);
 
+  //   Update the card with humidity
   var curHumidityEl = $("#curHumidityEl");
   curHumidityEl.text(`Humidity: ${curHumidity}%`);
 
+  //   Update the card with wind speed
   var curWindEl = $("#curWindEl");
   curWindEl.text(`Wind speed: ${roundSigFigs(curWindSpd, 1)}m/s`);
 
@@ -199,23 +200,28 @@ function renderCurrentConditions(
     curUVIEl.addClass("extreme-UVI");
   }
 
+  //   Make the results page visible once the content is ready to go
   var currentConditionsContainer = $("#currentConditionsContainer");
   currentConditionsContainer.css("display", "block");
 }
 
+// Helper function to update the forecast data cards
 function renderForecastConditions(data) {
-  // Loop through the indices of the forecast
+  // Loop through the indices of the forecast days to update them one by one
   for (i = 0; i < 5; i++) {
+    // Adjust the moment element used for each card
     var currentMoment = moment().add(i + 1, "days");
     var curDateEl = $(`#dateEl${i}`);
     curDateEl.text(currentMoment.format("MMMM Do"));
 
+    // Update the forecast weather icon
     var curIconEl = $(`#iconEl${i}`);
     curIconEl.attr(
       "src",
       `http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png`
     );
 
+    // Update the temperature with a rounded value
     var curTemperatureEl = $(`#temperatureEl${i}`);
     curTemperatureEl.text(
       `Temp(\u2103): ${roundSigFigs(
@@ -224,36 +230,18 @@ function renderForecastConditions(data) {
       )}(Day) / ${roundSigFigs(data.daily[i].temp.night, 1)}(Night)  `
     );
 
+    // Update the humidity value
     var curHumidityEl = $(`#humidityEl${i}`);
     curHumidityEl.text(`Humidity: ${data.daily[i].humidity}%`);
 
+    // Update the wind speed with a rounded value
     var curWindEl = $(`#windEl${i}`);
     curWindEl.text(
       `Wind speed: ${roundSigFigs(data.daily[i].wind_speed, 1)}m/s`
     );
   }
 
+  // Make the forecast conditions visible
   var forecastConditionsContainer = $("#forecastConditionsContainer");
   forecastConditionsContainer.css("display", "block");
 }
-
-//API call for forecast conditions
-// function getForecastConditions(cityName) {
-//     var weatherAPIKey = 'b8628e5537299dd92c268cb43152d77d';
-//     var apiUrlCityForecast = `https://api.openweathermap.org/data/3.0/forecast?q=${cityName}&appid=${WEATHER_API_KEY}`;
-
-//     fetch(apiUrlCityForecast)
-//         .then(function(response){
-//             console.log('-----\nRetrieving forecast weather...\n-----')
-//             console.log(response)
-//             var currentConditionsJSON = response.json()
-//             console.log(currentConditionsJSON)
-//         })
-
-// }
-
-// Populate current conditions
-
-// Populate single future forecast
-
-// Populate all 5 days of forecast
