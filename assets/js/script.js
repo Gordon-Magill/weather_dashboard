62c0d4fa68836954b0f8d4b91000f0dae255063e
Gordon-Magill <gordon.magill@gmail.com>
2022-09-05 23:30:54 -0700

    Improved sigfig rendering of data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
 
 var WEATHER_API_KEY = 'b8628e5537299dd92c268cb43152d77d';
 
+// Helper function for presenting data
+// Based off of https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-decimal-places-if-necessary
+function roundSigFigs(numberArg,sigFigs) {
+    return Math.round((numberArg+Number.EPSILON)*(10**sigFigs))/(10**sigFigs);
+}
+
 // Event handler for city search
 var btnSearchCities = $('#btnSearchCities');
 var formSearchCities = $('#formSearchCities');
@@ -118,7 +124,11 @@ function getExtendedUVData(data) {
 
 }
 
+
+
 function renderCurrentConditions(data,curTemp,curHumidity,curWindSpd,cityName,curUVI) {
+    
+    
     var cityNameTitle = $('#cityNameTitle')
     cityNameTitle.text(cityName);
 
@@ -129,33 +139,35 @@ function renderCurrentConditions(data,curTemp,curHumidity,curWindSpd,cityName,cu
     curIconEl.attr('src',`http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`)
     
     var curTemperatureEl = $('#curTemperatureEl')
-    curTemperatureEl.text(`Temperature: ${curTemp}\u2103`);
+    var roundedCurTemp = roundSigFigs(curTemp,1);
+    curTemperatureEl.text(`Temperature: ${roundedCurTemp}\u2103`);
 
     var curHumidityEl = $('#curHumidityEl');
     curHumidityEl.text(`Humidity: ${curHumidity}%`)
 
     var curWindEl = $('#curWindEl');
-    curWindEl.text(`Wind speed: ${curWindSpd}m/s`)
+    curWindEl.text(`Wind speed: ${roundSigFigs(curWindSpd,1)}m/s`)
 
     // Set UVI text and style based on its level to indicate severity
     var curUVIEl = $('#curUVIEl');
     curUVIEl.text(`UV Index: ${curUVI}/10`)
+    var roundedUVI = roundSigFigs(curUVI,1);
     curUVIEl.removeClass('vlow-UVI low-UVI mod-UVI high-UVI extreme-UVI');
     if (curUVI<1) {
-        curUVIEl.text(`UV Index: ${curUVI}/10 (Very low)`)
+        curUVIEl.text(`UV Index: ${roundedUVI}/10 (Very low)`)
         curUVIEl.removeClass('vlow-UVI low-UVI mod-UVI high-UVI extreme-UVI');
         curUVIEl.addClass('vlow-UVI')
     } else if (curUVI < 3) {
-        curUVIEl.text(`UV Index: ${curUVI}/10 (Low)`)
+        curUVIEl.text(`UV Index: ${roundedUVI}/10 (Low)`)
         curUVIEl.addClass('low-UVI')
     } else if (curUVI < 6) {
-        curUVIEl.text(`UV Index: ${curUVI}/10 (Moderate)`)
+        curUVIEl.text(`UV Index: ${roundedUVI}/10 (Moderate)`)
         curUVIEl.addClass('mod-UVI')
     } else if (curUVI < 9) {
-        curUVIEl.text(`UV Index: ${curUVI}/10 (High)`)
+        curUVIEl.text(`UV Index: ${roundedUVI}/10 (High)`)
         curUVIEl.addClass('high-UVI')
     } else if (curUVI > 9) {
-        curUVIEl.text(`UV Index: ${curUVI}/10 (Extreme)`)
+        curUVIEl.text(`UV Index: ${roundedUVI}/10 (Extreme)`)
         curUVIEl.addClass('extreme-UVI')
     }
     
@@ -176,13 +188,13 @@ function renderForecastConditions(data) {
         curIconEl.attr('src',`http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png`)
         
         var curTemperatureEl = $(`#temperatureEl${i}`)
-        curTemperatureEl.text(`Temp(\u2103): ${data.daily[i].temp.day}(Day) / ${data.daily[i].temp.night}(Night)  `);
+        curTemperatureEl.text(`Temp(\u2103): ${roundSigFigs(data.daily[i].temp.day,1)}(Day) / ${roundSigFigs(data.daily[i].temp.night,1)}(Night)  `);
     
         var curHumidityEl = $(`#humidityEl${i}`);
         curHumidityEl.text(`Humidity: ${data.daily[i].humidity}%`)
     
         var curWindEl = $(`#windEl${i}`);
-        curWindEl.text(`Wind speed: ${data.daily[i].wind_speed}m/s`)
+        curWindEl.text(`Wind speed: ${roundSigFigs(data.daily[i].wind_speed,1)}m/s`)
 
     }
 
